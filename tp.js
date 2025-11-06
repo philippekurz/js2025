09a9ad63d5bed341ef3bd474d4001d4c4567b62b
Philippe Kurz <philippe.kurz@gmail.com>
2025-11-06 15:48:08 +0100

    tp 1 et 2

---
+++ tp.js
@@ -0,0 +1,9 @@
+let a = 12;
+let b = 45;
+let c = a + b;
+console.log(c);
+
+let bonjour = 'Hello';
+let tout_le_monde = ' World!';
+let salutation = bonjour + tout_le_monde;
+console.log(salutation);
